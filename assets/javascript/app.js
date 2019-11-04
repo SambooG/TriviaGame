5e303f9ee0a0c322fb5ea630fcc43ad9dc43f534
Samantha Gaiser <sgaiser3@gmail.com>
2019-11-04 14:56:22 -0500

    adding a timer and pseudocode for corect answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,35 @@
-// start the game
-// make a timer
-// take away the answer and questions
-// time expires/
-    //show them the tally or right and wrong
-// 
+let timeRemaining = 5;
+let intervalId;
+let question1Answer = 'answer'
 
-// start the game 
-    // keep track of correct answers
-    // keep track of incorrect answers
-    // keep track of time left
-// timer runs out
-    // take away the questions
-    // show the ends result of answers
+
+function decrement() {
+    timeRemaining--;
+    $("#time-remaining").text(timeRemaining)
+    if(timeRemaining === 0){
+        stop()
+    }
+}
+
+function stop(){
+    clearInterval(intervalId);
+    // take away the questions --> set questions to not display by targeting using $(".question-container").hide()
+    // take away the timer --> set timer to not display by targeting "#time-remaining" similar to questions
+    // Get the answers the user selected for each question i.e. ->  $('input[name=question1-option]:checked').val();
     
+    // if the checked option matches the answer (repeat for all questions)
+        // Add a correct
+    // else 
+        // Add an incorrect
+    
+    // Show the user the correct
+    // Show the user the incorrect
+}  
+
+function startGame(){
+    $("#time-remaining").text(timeRemaining)
+    intervalId = setInterval(decrement, 1000);
+}
+
+
+startGame();
