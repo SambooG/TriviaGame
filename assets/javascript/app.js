865880bbbfc6bfa477e25b7f78c71bf0267552bc
Samantha Gaiser <sgaiser3@gmail.com>
2019-11-04 13:53:03 -0500

    adding html to project

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// start the game
+// make a timer
+// take away the answer and questions
+// time expires/
+    //show them the tally or right and wrong
+// 
+
+// start the game 
+    // keep track of correct answers
+    // keep track of incorrect answers
+    // keep track of time left
+// timer runs out
+    // take away the questions
+    // show the ends result of answers
+    
