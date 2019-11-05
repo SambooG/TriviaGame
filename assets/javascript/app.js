292f9d7c5c4ea3af72b6f0fab5607d62ac76baf1
Samantha Gaiser <sgaiser3@gmail.com>
2019-11-04 20:00:29 -0500

    adding the rest of the questions- working on restarting the timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
-let timeRemaining = 5;
+let timeRemaining = 40;
 let intervalId;
-let question1Answer = 'answer'
+let correct = 0
+let incorrect = 0
+let question1Answer = 'Wildwood'
 
 
 function decrement() {
@@ -13,23 +15,62 @@ function decrement() {
 
 function stop(){
     clearInterval(intervalId);
+    console.log(clearInterval)
     // take away the questions --> set questions to not display by targeting using $(".question-container").hide()
+    $(".question-container").hide()
     // take away the timer --> set timer to not display by targeting "#time-remaining" similar to questions
+    $("#time-remaining").hide()
     // Get the answers the user selected for each question i.e. ->  $('input[name=question1-option]:checked').val();
+    $("input[name= question1-option]:checked").val()
+    $("input[name= question2-option]:checked").val()
+    $("input[name= question3-option]:checked").val()
+    $("input[name= question4-option]:checked").val()
+    $("input[name= question5-option]:checked").val()
+    $("input[name= question6-option]:checked").val()
+    $("input[name= question7-option]:checked").val()
+    $("input[name= question8-option]:checked").val()
     
     // if the checked option matches the answer (repeat for all questions)
+    if("input[name= question1-option]:checked" = question1Answer){
         // Add a correct
-    // else 
-        // Add an incorrect
-    
-    // Show the user the correct
-    // Show the user the incorrect
+        correct++;
+        $("#correct-answers").text(correct);
+        // Show the user the correct
+    }
+        else if("input[name= question2-option]:checked" = question2Answer){
+            // Add a correct
+            correct++;
+            $("#correct-answers").text(correct);
+            // Show the user the correct
+        }
+        else if("input[name= question3-option]:checked" = question3Answer){
+            // Add a correct
+            correct++;
+            $("#correct-answers").text(correct);
+            // Show the user the correct
+        }
+        else if("input[name= question4-option]:checked" = question4Answer){
+            // Add a correct
+            correct++;
+            $("#correct-answers").text(correct);
+            // Show the user the correct
+        }
+            // else 
+            else{
+            // Add an incorrect
+                wrong++;
+                $("#wrong-answers").text(wrong);
+                // Show the user the incorrect
+        }
+     
 }  
 
-function startGame(){
-    $("#time-remaining").text(timeRemaining)
-    intervalId = setInterval(decrement, 1000);
-}
 
+    
+function startGame(){
+    $("#time-remaining").show();
+    $("#time-remaining").text(timeRemaining);
+    intervalId = setInterval(decrement, 1000); 
+}
 
 startGame();
